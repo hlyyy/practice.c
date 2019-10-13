5b4956350a13cdbf8ade7c54d2a304a7a782337e
黄凌云 <huanglingyun@huanglingyundeMacBook-Air.local>
2019-10-13 17:20:26 +0800

    modify

--- 6-2.c
+++ 6-2.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+
 int min3 (int a,int b,int c)
 {	int min=a;
 	if (b<min) min=b;
@@ -9,9 +10,9 @@ int min3 (int a,int b,int c)
 int main(void)
 {	int x,y,z;
 	printf("请输入三个整数\n");
-	printf("x的值为:");scanf("%d",&x);
-	printf("y的值为:");scanf("%d",&y);
-	printf("z的值为:");scanf("%d",&z);
+	printf("x的值为:"); scanf("%d",&x);
+	printf("y的值为:"); scanf("%d",&y);
+	printf("z的值为:"); scanf("%d",&z);
 	printf("三个数中最小的是%d\n",min3(x,y,z));
 	return 0;
 }
